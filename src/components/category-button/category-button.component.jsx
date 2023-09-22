470e35e302f45d4d6c3cfbabf447896b80a4f960
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-09-22 14:58:55 +0200

    Applied click sound to buttons

--- src/components/category-button/category-button.component.jsx
+++ src/components/category-button/category-button.component.jsx
@@ -1,7 +1,17 @@
 import "./category-button.styles.css";
+import clickSound from "../../assets/sounds/app-sounds/click-sound.wav";
 
 const CategoryButton = (props) => {
+  const playSound = () => {
+    const audio = new Audio(clickSound);
+    audio.play();
+  };
+
   const { buttonText, className } = props;
-  return <button className={`button-55 ${className}`}>{buttonText}</button>;
+  return (
+    <button onClick={playSound} className={`button-55 ${className}`}>
+      {buttonText}
+    </button>
+  );
 };
 export default CategoryButton;
